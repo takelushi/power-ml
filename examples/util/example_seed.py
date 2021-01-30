4a38cf52604dc872d4083483d7d06102819b4318
Takeru Saito <takelushi@gmail.com>
2021-01-31 02:43:57 +0900

    add example of seed

--- examples/util/example_seed.py
+++ examples/util/example_seed.py
@@ -2,14 +2,14 @@
 
 import random
 
-from power_ml.util import seed
+from power_ml.util.seed import set_seed, set_seed_random
 
 for v in [5, 5, 6]:
-    seed.set_seed(v)
+    set_seed(v)
     print('Seed: {}'.format(v))
     print(random.random())
     print(random.random())
 
-v = seed.set_seed_random()
+v = set_seed_random()
 print('Seed: {}'.format(v))
 print(random.random())
